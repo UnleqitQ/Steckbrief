42234b2109643a3de0b2184e69d3cc85194e81cf
UnleqitQ <quentin.weinbrenner@gmail.com>
2022-01-13 18:12:07 +0100

    Initial commit

---
+++ src/main/java/de/redfox/steckbrief/Steckbrief.java
@@ -0,0 +1,18 @@
+package de.redfox.steckbrief;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Steckbrief extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		// Plugin startup logic
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		// Plugin shutdown logic
+	}
+	
+}
