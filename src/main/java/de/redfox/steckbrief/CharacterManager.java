13f3e5b86fe22020d348f223e33cd3f6681467ac
UnleqitQ <quentin.weinbrenner@gmail.com>
2022-01-13 19:30:34 +0100

    Update

--- src/main/java/de/redfox/steckbrief/CharacterManager.java
+++ src/main/java/de/redfox/steckbrief/CharacterManager.java
@@ -1,13 +1,47 @@
 package de.redfox.steckbrief;
 
+import com.google.gson.JsonElement;
+import de.redfox.steckbrief.config.ConfigManager;
+
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 
-public class CharacterManager {
+public final class CharacterManager {
 	
 	public static Map<UUID, CharacterDescription> characters = new HashMap<>();
 	
 	public static Map<UUID, PlayerInformation> players = new HashMap<>();
+	
+	private CharacterManager() {}
+	
+	public static void loadCharacters() {
+		Set<Map.Entry<String, JsonElement>> entries = ConfigManager.characters.rootSection.entrySet();
+		for (Map.Entry<String, JsonElement> entry : entries) {
+			CharacterDescription characterDescription = CharacterDescription.load(entry.getValue().getAsJsonObject());
+			characters.put(characterDescription.uuid, characterDescription);
+		}
+	}
+	
+	public static void loadPlayers() {
+		for (String key : ConfigManager.players.rootSection.keySet()) {
+			PlayerInformation playerInformation = PlayerInformation.load(ConfigManager.players,
+					UUID.fromString(key));
+			players.put(playerInformation.player, playerInformation);
+		}
+	}
+	
+	public static void savePlayer(UUID player) {
+		if (players.containsKey(player)) {
+			players.get(player).save(ConfigManager.players);
+		}
+	}
+	
+	public static void saveCharacter(UUID character) {
+		if (characters.containsKey(character)) {
+			characters.get(character).save(ConfigManager.characters);
+		}
+	}
 	
 }
