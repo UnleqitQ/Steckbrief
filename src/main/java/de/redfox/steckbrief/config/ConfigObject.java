d668ccdd9dc4f05848cc6ce5002a49d4bb3ba5a4
UnleqitQ <quentin.weinbrenner@gmail.com>
2022-01-13 20:12:37 +0100

    Update

--- src/main/java/de/redfox/steckbrief/config/ConfigObject.java
+++ src/main/java/de/redfox/steckbrief/config/ConfigObject.java
@@ -16,72 +16,78 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class ConfigObject {
-    public JsonObject rootSection;
-
-    public File file;
-    private final Path path;
-
-    public ConfigObject(String path, String file) {
-        this.path = Paths.get(path, file);
-        create();
-        load();
-    }
-
-    public void set(String property, JsonElement value) {
-        String[] sections = property.split("\\.");
-
-        JsonObject currentObject = rootSection;
-        for (int i = 0; i < sections.length; i++) {
-            String sectionKey = sections[i];
-            JsonElement tempObject = currentObject.get(sectionKey);
-            if (tempObject == null) {
-                tempObject = new JsonObject();
-                currentObject.add(sectionKey, tempObject);
-            }
-
-            currentObject = tempObject.getAsJsonObject();
-            if (i == sections.length - 1) {
-                currentObject.add(sectionKey, value);
-            }
-        }
-    }
-
-    public void create() {
-        file = new File(path.getParent().toUri());
-        file.mkdirs();
-        file = new File(path.toUri());
-        try {
-            file.createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void load() {
-        try {
-            SeekableByteChannel sbc = Files.newByteChannel(path);
-            InputStream in = Channels.newInputStream(sbc);
-
-            StringBuilder read = new StringBuilder((int) sbc.size());
-            int res;
-            while ((res = in.read()) != -1) {
-                read.append(res);
-            }
-            rootSection = JSONParser.parse(read.toString());
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public void save() {
-        try {
-            FileOutputStream stream = new FileOutputStream(file);
-            stream.write(JSONParser.toString(rootSection).getBytes(StandardCharsets.UTF_8));
-            stream.flush();
-            stream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
+	
+	public JsonObject rootSection;
+	
+	public File file;
+	private final Path path;
+	
+	public ConfigObject(String path, String file) {
+		this.path = Paths.get(path, file);
+		create();
+		load();
+	}
+	
+	public void set(String property, JsonElement value) {
+		String[] sections = property.split("\\.");
+		
+		JsonObject currentObject = rootSection;
+		for (int i = 0; i < sections.length; i++) {
+			String sectionKey = sections[i];
+			JsonElement tempObject = currentObject.get(sectionKey);
+			if (tempObject == null) {
+				tempObject = new JsonObject();
+				currentObject.add(sectionKey, tempObject);
+			}
+			
+			currentObject = tempObject.getAsJsonObject();
+			if (i == sections.length - 1) {
+				currentObject.add(sectionKey, value);
+			}
+		}
+	}
+	
+	public void create() {
+		file = new File(path.getParent().toUri());
+		file.mkdirs();
+		file = new File(path.toUri());
+		try {
+			file.createNewFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void load() {
+		try {
+			SeekableByteChannel sbc = Files.newByteChannel(path);
+			InputStream in = Channels.newInputStream(sbc);
+			
+			StringBuilder read = new StringBuilder((int) sbc.size());
+			int res;
+			while ((res = in.read()) != -1) {
+				read.append(res);
+			}
+			rootSection = JSONParser.parse(read.toString());
+			
+			if (rootSection == null) {
+				rootSection = new JsonObject();
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void save() {
+		try {
+			FileOutputStream stream = new FileOutputStream(file);
+			stream.write(JSONParser.toString(rootSection).getBytes(StandardCharsets.UTF_8));
+			stream.flush();
+			stream.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
