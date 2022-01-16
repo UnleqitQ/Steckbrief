0a77d0961bd2ba2a7fe516b6b08577c05d0e3649
UnleqitQ <quentin.weinbrenner@gmail.com>
2022-01-16 21:31:50 +0100

    Update

--- src/main/java/de/redfox/steckbrief/CharacterManager.java
+++ src/main/java/de/redfox/steckbrief/CharacterManager.java
@@ -1,5 +1,7 @@
 package de.redfox.steckbrief;
 
+import com.earth2me.essentials.Essentials;
+import com.earth2me.essentials.User;
 import com.google.gson.JsonElement;
 import de.redfox.steckbrief.manager.config.ConfigManager;
 import org.bukkit.Bukkit;
@@ -20,6 +22,7 @@ import java.util.UUID;
 
 public final class CharacterManager implements Listener, Runnable {
 	
+	public Essentials essentials = (Essentials) Bukkit.getPluginManager().getPlugin("EssentialsX");
 	public static Map<UUID, CharacterDescription> characters = new HashMap<>();
 	public static Map<String, UUID> characterNames = new HashMap<>();
 	public static Map<UUID, PlayerInformation> players = new HashMap<>();
@@ -81,11 +84,13 @@ public final class CharacterManager implements Listener, Runnable {
 		Player player = event.getPlayer();
 		if (hasAliveCharacter(player.getUniqueId())) {
 			CharacterDescription character = characters.get(players.get(player.getUniqueId()).characters.get(0));
-			player.setDisplayName(ChatColor.BLUE + character.firstname + " " + character.lastname + ChatColor.WHITE);
+			player.setDisplayName(ChatColor.BLUE + character.getName() + ChatColor.WHITE);
 			player.setPlayerListName(
-					ChatColor.BLUE + character.firstname + " " + character.lastname + ChatColor.GRAY + " (" + player.getName() + ")");
-			player.setCustomName(ChatColor.BLUE + character.firstname + " " + character.lastname + ChatColor.WHITE);
+					ChatColor.BLUE + character.getName() + ChatColor.GRAY + " (" + player.getName() + ")");
+			player.setCustomName(ChatColor.BLUE + character.getName() + ChatColor.WHITE);
 			player.setCustomNameVisible(true);
+			User user = essentials.getUser(player);
+			user.setNickname(ChatColor.BLUE + character.getName());
 			player.sendMessage(
 					ChatColor.GREEN + "You joined as " + ChatColor.GOLD + character.firstname + " " + character.lastname);
 		}
