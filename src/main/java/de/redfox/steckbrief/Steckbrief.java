b50f35e618c638feb5de8ae91e08b314d93c2932
UnleqitQ <quentin.weinbrenner@gmail.com>
2022-01-19 20:56:04 +0100

    Update

--- src/main/java/de/redfox/steckbrief/Steckbrief.java
+++ src/main/java/de/redfox/steckbrief/Steckbrief.java
@@ -4,7 +4,12 @@ import de.redfox.steckbrief.commands.RoleplayCommand;
 import de.redfox.steckbrief.manager.PlaceholderManger;
 import de.redfox.steckbrief.manager.blindness.BlindnessManager;
 import de.redfox.steckbrief.manager.config.ConfigManager;
+import de.redfox.steckbrief.tobemoved.FirstJoinSession;
 import net.arcaniax.headdisplays.HeadDisplays;
+import net.arcaniax.headdisplays.display.Display;
+import net.arcaniax.headdisplays.display.UpdateFrequency;
+import net.arcaniax.headdisplays.util.Cuboid;
+import net.arcaniax.headdisplays.util.Direction;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -35,7 +40,11 @@ public final class Steckbrief extends JavaPlugin {
 		
 		placeholderManger = new PlaceholderManger();
 		placeholderManger.register();
-		HeadDisplays.getDisplayManger().addDisplay();
+		Display display = new Display(HeadDisplays.getDisplayManger().getNextDisplayId(),
+				Cuboid.fromRadius(FirstJoinSession.spawnLoc.clone().add(2, 1, 0), 4), UpdateFrequency.SECONDS_1, true,
+				true, false, true, "%steckbrief_1%", 10, FirstJoinSession.spawnLoc.clone().add(2, 1, 0),
+				Direction.NORTH);
+		HeadDisplays.getDisplayManger().addDisplay(display);
 	}
 	
 	public <T extends CommandExecutor> void registerCommand(String cmd, T handler) {
--- src/main/java/de/redfox/steckbrief/tobemoved/FirstJoinSession.java
+++ src/main/java/de/redfox/steckbrief/tobemoved/FirstJoinSession.java
@@ -14,66 +14,67 @@ import org.bukkit.plugin.RegisteredListener;
 import org.bukkit.plugin.SimplePluginManager;
 
 public class FirstJoinSession {
-    private static Location startLoc = new Location(Bukkit.getWorlds().get(0), 0, 65, 0, 0, 0);
-    private static Location spawnLoc = new Location(Bukkit.getWorlds().get(0), 10, 65, 0, 0, 0);
-
-    private Player player;
-
-    private LocalListener activeListener;
-
-    public FirstJoinSession(Player player) {
-        this.player = player;
-        activeListener = new LocalListener();
-        Bukkit.getPluginManager().registerEvents(activeListener, Steckbrief.getInstance());
-        start();
-
-        Steckbrief.getHeadDisplays().getf
-    }
-
-    public void start() {
-        player.setGameMode(GameMode.SPECTATOR);
-        player.setFlying(true);
-        player.teleport(startLoc);
-
-        SimplePluginManager pluginManager = (SimplePluginManager) Bukkit.getPluginManager();
-        HandlerList field = new ReflectionSession(pluginManager)
-                .getField("", HandlerList.class);
-
-        for (RegisteredListener registeredListener : field.getRegisteredListeners()) {
-            new ReflectionSession(registeredListener)
-                    .getMethodWithAnnotation(EventHandler.class);
-
-        }
-    }
-
-    public void stop() {
-        HandlerList.unregisterAll(activeListener);
-
-        player.setGameMode(GameMode.SURVIVAL);
-        player.setFlying(false);
-        player.teleport(spawnLoc);
-    }
-
-    private class LocalListener implements Listener {
-        @EventHandler
-        public void onEvent(PlayerMoveEvent event) {
-
-            if (!event.getPlayer().getUniqueId().equals(player.getUniqueId()))
-                return;
-
-            Location from = event.getFrom();
-            Location to = event.getTo();
-            double x = from.getX();
-            double y = from.getY();
-            double z = from.getZ();
-
-            from.setYaw(to.getYaw());
-            from.setPitch(to.getPitch());
-
-            if (x != to.getX() || y != to.getY() || z != to.getZ()) {
-                event.setTo(from);
-            }
-        }
-    }
-
+	
+	public static Location startLoc = new Location(Bukkit.getWorlds().get(0), 0, 65, 0, 0, 0);
+	public static Location spawnLoc = new Location(Bukkit.getWorlds().get(0), 10, 65, 0, 0, 0);
+	
+	private Player player;
+	
+	private LocalListener activeListener;
+	
+	public FirstJoinSession(Player player) {
+		this.player = player;
+		activeListener = new LocalListener();
+		Bukkit.getPluginManager().registerEvents(activeListener, Steckbrief.getInstance());
+		start();
+	}
+	
+	public void start() {
+		player.setGameMode(GameMode.SPECTATOR);
+		player.setFlying(true);
+		player.teleport(startLoc);
+		
+		SimplePluginManager pluginManager = (SimplePluginManager) Bukkit.getPluginManager();
+		HandlerList field = new ReflectionSession(pluginManager)
+				.getField("", HandlerList.class);
+		
+		for (RegisteredListener registeredListener : field.getRegisteredListeners()) {
+			new ReflectionSession(registeredListener)
+					.getMethodWithAnnotation(EventHandler.class);
+			
+		}
+	}
+	
+	public void stop() {
+		HandlerList.unregisterAll(activeListener);
+		
+		player.setGameMode(GameMode.SURVIVAL);
+		player.setFlying(false);
+		player.teleport(spawnLoc);
+	}
+	
+	private class LocalListener implements Listener {
+		
+		@EventHandler
+		public void onEvent(PlayerMoveEvent event) {
+			
+			if (!event.getPlayer().getUniqueId().equals(player.getUniqueId()))
+				return;
+			
+			Location from = event.getFrom();
+			Location to = event.getTo();
+			double x = from.getX();
+			double y = from.getY();
+			double z = from.getZ();
+			
+			from.setYaw(to.getYaw());
+			from.setPitch(to.getPitch());
+			
+			if (x != to.getX() || y != to.getY() || z != to.getZ()) {
+				event.setTo(from);
+			}
+		}
+		
+	}
+	
 }
